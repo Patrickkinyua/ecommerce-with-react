8f098e5ab855ab44b583ed07e6801a5d43937c70
patrick-kinyua <patrickkinyua2004@gmail.com>
2026-09-19 01:23:55 +0300

    use data from backend

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import Header from "../components/Header";
 import {products} from '../../starting-code/data/products'
 import "./HomePage.css";
+import axios from 'axios'
+import {useEffect,useState} from 'react'
 function HomePage() {
+  const [products,setProducts]=useState([])
+  useEffect(()=>{
+   axios.get('http://localhost:3000/api/products')
+   .then((response)=> {
+    setProducts(response.data)
+
+   })
+
+  },[])
   return (
     <>
     <Header/>
